db35e36ace88b782e12d1ccedabf5fe1b6a50f50
thuanpt <tienthuan05082002@gmail.com>
2024-05-16 01:54:10 +0700

    update compose file

--- mongorestore.sh
+++ mongorestore.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-mongo --eval "db.dropDatabase()"
-mongorestore --drop
+mongorestore
 # Kiểm tra kết quả lệnh mongorestore
 if [ $? -eq 0 ]; then
   echo "MongoDB restore completed successfully."
